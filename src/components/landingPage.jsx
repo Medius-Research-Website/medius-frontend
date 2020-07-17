f6da4f99fbf5670488ddb7318fb2583b9e218fa9
Timothy Park <tpark464@bu.edu>
2020-07-17 12:40:38 -0700

    test commit

---
+++ src/components/landingPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Medius Research</h1>
+        <body>Welcome to Medius Research!</body>
+      </React.Fragment>
+    );
+  }
+}
+
+export default LandingPage;
