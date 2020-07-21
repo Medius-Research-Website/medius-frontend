2505bd7b30b5990e5a2dca578994648fb6b4ff0b
Anchita Bora <anchita.bora@gmail.com>
2020-07-20 20:01:27 -0700

    Update and rename App.js --> App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import home from './components/home';
+import about from './components/about';
+import Navbar from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar/>
+        <Route exact path="/" component={home} />
+        <Route exact path="/about" component={about} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
