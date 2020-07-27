dd56f3352c0f385e519f27fb05cf5e647cb11b68
Anchita Bora <anchita.bora@gmail.com>
2020-07-27 09:51:43 -0700

    Update App.jsx with userprofile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import home from './components/home';
 import about from './components/about';
+import profile from './components/userprofile';
 import Navbar from './components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,9 +14,11 @@ function App() {
         <Navbar/>
         <Route exact path="/" component={home} />
         <Route exact path="/about" component={about} />
+        <Route exact path="/userprofile" component={profile} />
       </div>
     </Router>
   );
 }
 
 export default App;
+
