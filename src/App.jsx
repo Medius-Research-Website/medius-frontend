9a1e5dce4d7574702a7a350d5d1ac3a57420c3ea
Timothy Park <tpark464@bu.edu>
2020-07-30 14:17:27 -0700

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import home from "./components/home";
 import about from "./components/about";
-import Navbar from "./components/navbar";
+//import Navbar from "./components/navbar";
 import userprofile from "./components/userprofile";
 import Signup from "./components/signup";
 import Signin from "./components/signin";
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <div>
-      {/* <Navbar/> */}
+        {/* <Navbar /> */}
         <Route exact path="/" component={home} />
         <Route exact path="/about" component={about} />
         <Route exact path="/userprofile" component={userprofile} />
