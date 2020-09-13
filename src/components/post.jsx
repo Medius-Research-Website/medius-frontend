6eaf88127667a58f3ce3d2c2378943c383b59ba4
winkhanh <giakhanhnk2000@gmail.com>
2020-09-13 00:57:15 -0400

    make a test data to develop UI

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,31 +1,26 @@
 import React from "react";
+
 import "./css_files/post.scss";
 
 export default function Post({ post }) {
   return (
-        <div className="main">
-          <div>
-            
-          </div>
-          <div className="sidebar">
-            <div className="ticker">Ticker: {post.ticker} </div>
-            <div className="percentage-change">100%</div>
-            <button className="like">
-              <i className="arrow-up up" aria-hidden="true"></i>
-            </button>
-            <button className="dislike">
-              <i className="arrow-down down" aria-hidden="true"></i>
-            </button>
-            <div className="date">Date</div>
-          </div>
-          <div className="information">
-            <div className="title">
-              {post.idea}
-            </div>
-            <p>
-              {post.insight}
-            </p>
-          </div>
-        </div>
+    <div className="feed__post">
+      <div className="feed__post__left">
+        <p>{post.ticker} </p>
+        <p>Company: {post.industry}</p>
+        <div className="bubble--sell">Sell/Buy</div>
+      </div>
+      
+      <div className="feed__post__right">
+        <h5 className="title">
+          {post.idea}
+        </h5>
+        <p>
+        a
+          {post.insight}
+        </p>
+      </div>
+
+    </div>
   );
 }
