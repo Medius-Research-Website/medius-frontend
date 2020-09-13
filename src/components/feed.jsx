812a7ab2b8fc9d599441bc65c1cb284ea16f81d4
winkhanh <giakhanhnk2000@gmail.com>
2020-09-13 02:05:18 -0400

    add SCSS for landing page and feed

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -12,6 +12,22 @@ const testData=[
     insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
     idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
     oldPrice:3
+  },
+  {
+    ticker:"TSN",//symbol
+    industry:"Tyson",//company
+    sector:"Test sector",
+    insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
+    idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
+    oldPrice:3
+  },
+  {
+    ticker:"TSN",//symbol
+    industry:"Tyson",//company
+    sector:"Test sector",
+    insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
+    idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
+    oldPrice:3
   }
 ]
 class Feed extends Component {
@@ -26,8 +42,8 @@ class Feed extends Component {
       return (
         <React.Fragment>
           {//this.props.all.map((post) => (
-            testData.map((post) => (
-            <Link to={`posts/${post.id}`} style={{ textDecoration: 'none' }}>
+            testData.map((post,idx) => (
+            <Link to={`posts/${post.id}`} style={{ textDecoration: 'none' }} key={idx}>
               <Post post={post} />
             </Link>
           ))}
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import Feed from "../components/feed";
 import Navbar from "../components/navbar";
-
+import "../components/css_files/landingpage.scss";
 class LandingPage extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
-        <Navbar />
-        <Feed />
+        <Navbar  />
+        <div className="landing-page">
+          <div className="profile--bubble"/>
+          <div className="feed">
+            <Feed />
+          </div>
+          <div className="trending"/>
+        </div>
+        
       </React.Fragment>
     );
   }
