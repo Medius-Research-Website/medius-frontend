18d191341753f117e94f73340e99d6023544a15f
winkhanh <giakhanhnk2000@gmail.com>
2020-09-13 01:38:50 -0400

    add SCSS for post

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -6,19 +6,23 @@ export default function Post({ post }) {
   return (
     <div className="feed__post">
       <div className="feed__post__left">
-        <p>{post.ticker} </p>
-        <p>Company: {post.industry}</p>
+        <p className="ticker">{post.ticker} </p>
+        <p className="company">Company: {post.industry}</p>
         <div className="bubble--sell">Sell/Buy</div>
       </div>
       
       <div className="feed__post__right">
-        <h5 className="title">
-          {post.idea}
-        </h5>
-        <p>
-        a
-          {post.insight}
-        </p>
+        <div className="interaction-stat">
+          Like/Comment placeholder
+        </div>
+        <div className="content">
+          <p className="post-title">
+            {post.idea}
+          </p>
+          <p className="info">
+            {post.insight}
+          </p>
+        </div>
       </div>
 
     </div>
