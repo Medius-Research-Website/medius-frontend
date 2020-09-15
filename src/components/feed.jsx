72b7b3a9758ba1e5dbdbf4a0b7efeff4920b30ae
winkhanh <giakhanhnk2000@gmail.com>
2020-09-15 11:55:37 -0400

    clear testing dump data

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -3,7 +3,7 @@ import Post from "./post";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
-
+/* 
 const testData=[
   {
     ticker:"TSN",//symbol
@@ -29,7 +29,7 @@ const testData=[
     idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
     oldPrice:3
   }
-]
+] */
 class Feed extends Component {
   componentDidMount() {
     this.props.fetchPosts();
@@ -38,11 +38,10 @@ class Feed extends Component {
   // access posts through this.props.allPosts; display iteratively through .map()
   render() {
     console.log(this.props.all.length);
-    if (this.props.all.length === 0) {
+    if (this.props.all.length !== 0) {
       return (
         <React.Fragment>
-          {//this.props.all.map((post) => (
-            testData.map((post,idx) => (
+          {this.props.all.map((post) => ( 
             <Link to={`posts/${post.id}`} style={{ textDecoration: 'none' }} key={idx}>
               <Post post={post} />
             </Link>
