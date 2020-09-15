8fd36cdc65f5b808b0a68be72e05656b3c8bd33e
winkhanh <giakhanhnk2000@gmail.com>
2020-09-15 11:52:34 -0400

    Add Title and Input box on feed page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Feed from "../components/feed";
 import Navbar from "../components/navbar";
 import "../components/css_files/landingpage.scss";
+import FeedHead from "../components/feedhead";
 class LandingPage extends Component {
   state = {};
   render() {
@@ -11,6 +12,7 @@ class LandingPage extends Component {
         <div className="landing-page">
           <div className="profile--bubble"/>
           <div className="feed">
+            <FeedHead/>
             <Feed />
           </div>
           <div className="trending"/>
