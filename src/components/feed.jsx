6b8884b18a4319a880df2b100f6ae597e596c4d4
winkhanh <giakhanhnk2000@gmail.com>
2020-09-19 22:44:50 -0400

    add show/hide comment function

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -3,33 +3,7 @@ import Post from "./post";
 import { connect } from "react-redux";
 import { withRouter, Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
-/* 
-const testData=[
-  {
-    ticker:"TSN",//symbol
-    industry:"Tyson",//company
-    sector:"Test sector",
-    insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
-    idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
-    oldPrice:3
-  },
-  {
-    ticker:"TSN",//symbol
-    industry:"Tyson",//company
-    sector:"Test sector",
-    insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
-    idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
-    oldPrice:3
-  },
-  {
-    ticker:"TSN",//symbol
-    industry:"Tyson",//company
-    sector:"Test sector",
-    insight:"tyson received a price target of$83 per share ty ispan asd jasdadk sadks asd saj ajcajs  adjakhas sadsakjhs adaskhsa dsajhdaksdjh sdasdasskd skdjasdk asds",
-    idea:"Tyson remains a secure investment as the stock prize has not improved much due to ..",
-    oldPrice:3
-  }
-] */
+
 class Feed extends Component {
   componentDidMount() {
     this.props.fetchPosts();
@@ -42,9 +16,7 @@ class Feed extends Component {
       return (
         <React.Fragment>
           {this.props.all.map((post) => ( 
-            <Link to={`posts/${post.id}`} style={{ textDecoration: 'none' }} key={post.id}>
-              <Post post={post} />
-            </Link>
+              <Post post={post} key={post.id}/>
           ))}
         </React.Fragment>
       );
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 
 import "./css_files/post.scss";
 
 
 // this is the small view of a post for the feed page
 export default function Post({ post }) {
+  const [showComment,setShowComment]=useState(false);
+  const onCommentToggle = (e)=>{
+    e.stopPropagation();
+    setShowComment(prev=>!prev);
+  }
   return (
     <div className="feed__post">
       <div className="feed__post__left">
@@ -14,16 +19,28 @@ export default function Post({ post }) {
       </div>
       
       <div className="feed__post__right">
-        <div className="interaction-stat">
-          Like/Comment placeholder
+        <div className="feed__post__right__content">
+          <div className="interaction-stat">
+            Like/Comment placeholder
+          </div>
+          <div className="content">
+            <p className="post-title">
+              {post.idea}
+            </p>
+            <p className="info">
+              {post.insight}
+            </p>
+          </div>
         </div>
-        <div className="content">
-          <p className="post-title">
-            {post.idea}
-          </p>
-          <p className="info">
-            {post.insight}
-          </p>
+        <div className="feed__post__right__comment">
+          {showComment
+            ?(<div>
+            {/*show comments here*/}
+              <p onClick={onCommentToggle} className="comment-toggle">hide comments</p>
+            </div>)
+            :(
+              <p onClick={onCommentToggle} className="comment-toggle">view more comments</p>
+          )}
         </div>
       </div>
 
