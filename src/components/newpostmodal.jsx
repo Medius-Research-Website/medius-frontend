d1d77498ef80f69c02672cd937820079f2598c25
winkhanh <giakhanhnk2000@gmail.com>
2020-09-20 04:46:52 -0400

    Create template Modal

--- src/components/newpostmodal.jsx
+++ src/components/newpostmodal.jsx
@@ -3,11 +3,25 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class NewPostModal extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            ticker:"",
+            insight:"",
+            idea:""
+        }
+    }
+    onSubmit(){
+        //handleSubmission
+    }
     render(){
         if (this.props.show)
             return(
                 <div>
-                Modal
+                    <input value={this.state.ticker} onChange={(e)=>{this.setState({ticker:e.target.value||""})}} type="text"/>
+                    <input value={this.state.insight} onChange={(e)=>{this.setState({insight:e.target.value||""})}} type="text"/>
+                    <input value={this.state.idea} onChange={(e)=>{this.setState({idea:e.target.value||""})}} type="text"/>
+                    <button onClick={this.onSubmit}>Submit</button>
                 </div>
             )
         else
