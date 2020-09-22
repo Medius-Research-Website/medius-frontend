b5f0dbae4549ac771c423e8c69ff8c5129a0a106
caroline tornquist <caroline.22@dartmouth.edu>
2020-09-22 13:57:37 -0400

    updating so it's back to typeform

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import ReactGA from 'react-ga';
 import Home from "./components/home";
-import about from "./components/about";
+import history from './history'
+/* import about from "./components/about";
 import userprofile from "./components/userprofile";
 import Signup from "./components/signup";
 import Signin from "./components/signin";
@@ -11,8 +12,7 @@ import PrivateRoute from './components/privateroute';
 import LandingPage from "./pages/landingPage";
 import Community from "./pages/communityPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import history from './history'
-import singlepost from "./components/singlepost";
+import singlepost from "./components/singlepost";*/
 
 const trackingId = "UA-176041306-1"
 ReactGA.initialize(trackingId);
@@ -25,13 +25,13 @@ function App() {
       <div>
         {/* <Navbar /> */}
           <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={about} />
+          {/*<Route exact path="/about" component={about} />
           <PrivateRoute exact path="/userprofile" component={userprofile} />
           <Route exact path="/signup" component={Signup} />
           <PrivateRoute exact path="/signin" component={Signin} />
           <Route exact path="/landingpage" component={LandingPage}/>
           <Route exact path="/communitypage" component={Community} />
-          <Route exact path="/posts/:postID" component={singlepost} />
+  <Route exact path="/posts/:postID" component={singlepost} /> */}
       </div>
     </Router>
   );
