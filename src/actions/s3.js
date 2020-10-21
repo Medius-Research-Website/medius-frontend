7f6853b69a106e3829d4d7e089a322557bfd6594
caroline tornquist <caroline.22@dartmouth.edu>
2020-10-21 18:17:58 -0400

    changed to heroku

--- src/actions/s3.js
+++ src/actions/s3.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const ROOT_URL = 'http://localhost:9090/api';
-// const ROOT_URL = 'https://medius-api.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://medius-api.herokuapp.com/api';
 
 // THIS IS THE ONLY FUNCTION YOU SHOULD CALL - THE OTHER TWO ARE HELPERS
 export function uploadFile(file) {
