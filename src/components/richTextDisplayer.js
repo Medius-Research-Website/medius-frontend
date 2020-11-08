7049e6be41c14396d1ea25bb88fb0938d0ae25c2
winkhanh <giakhanhnk2000@gmail.com>
2020-11-07 19:27:03 -0500

    add children prop for displayer

--- src/components/richTextDisplayer.js
+++ src/components/richTextDisplayer.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {Editor, convertFromRaw, EditorState} from 'draft-js';
 
-const RichTextDisplayer = ({value, className})=>{
-    
-    //console.log(value)
-            //console.log(value);
+const RichTextDisplayer = ({value, className, children})=>{
     try{
-
-    
-    let content=JSON.parse(value);
-    console.log(content);
-    console.log(convertFromRaw(content));
-    const editorState=EditorState.createWithContent(convertFromRaw(content));
-    console.log(editorState);
-    return (<div className={`${className}`}>
-    <Editor editorState={editorState} 
-    readOnly={true}
-    />
-    </div>);    
-    }catch(error){
+        
+        let content=JSON.parse(children?children:value);
+        console.log(content);
+        console.log(convertFromRaw(content));
+        const editorState=EditorState.createWithContent(convertFromRaw(content));
+        console.log(editorState);
+        return (<div className={`${className}`}>
+            <Editor editorState={editorState} 
+            readOnly={true}
+            />
+            </div>);    
+        }catch(error){
         console.log("Not in rich text format");
         return(
             <div className={className}>{value}</div>
